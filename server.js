c233e48e2ed78e76517a9f7efe15dbd98f7aee4d
MJaurigue <jaurigue.michael@gmail.com>
2023-05-09 23:22:41 -0500

    added react.js

--- server.js
+++ server.js
@@ -11,16 +11,15 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use(require("./routes/record"));
+app.use(require("./server/routes/record"));
 
 // Get MongoDB driver connection
-const dbo = require("./db/conn");
- 
+const dbo = require("./server/db/conn");
+
 app.listen(port, () => {
   // Perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
- 
   });
   console.log(`Server is running on port: ${port}`);
 });
